af941c33f42359b0e2d87b03f0328bdf19db05d8
a1ex.solovyov <a1ex.solovyov10@gmail.com>
2023-06-20 21:29:17 +0300

    Initial Commit

---
+++ THE LIFE_V_0.1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  THE LIFE_V_0.1
+//
+//  Created by a1ex on 6/20/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
